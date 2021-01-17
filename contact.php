6e432edeb77601c4b7729289a04350b599005d39
Jerespy <4helpteam@gmail.com>
2021-01-17 06:54:14 +0100

    updates on files

---
+++ contact.php
@@ -0,0 +1,367 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+<!-- Basic Page Needs
+================================================== -->
+<title>Hostel Allocation</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!-- CSS
+================================================== -->
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/colors/blue.css">
+<link rel="shortcut icon" type="images/jpg" href="images/unilogo.jpg">
+</head>
+<body>
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+<!-- Header Container
+================================================== -->
+<header id="header-container" class="fullwidth">
+
+	<!-- Header -->
+	<div id="header">
+		<div class="container">
+			
+			<!-- Left Side Content -->
+			<div class="left-side">
+				
+				<!-- Logo -->
+				<div id="logo">
+					<a href="index-2.html"><img src="images/unilogo.jpg" alt=""></a>
+				</div>
+
+				<!-- Main Navigation -->
+				<nav id="navigation" style="float:right">
+					<ul id="responsive">
+						<li><a href="index-2.html" class="current">Home</a></li>
+						<li><a href="hostels.html">Hostels</a></li>
+						<li><a href="pages-contact.html">Contacts</a></li>
+						<li><a href="pages-register.html">Register</a></li>
+						<li><a href="pages-login.html">Login</a></li>
+
+					</ul>
+				</nav>
+				<span class="mmenu-trigger">
+					<button class="hamburger hamburger--collapse" type="button">
+						<span class="hamburger-box">
+							<span class="hamburger-inner"></span>
+						</span>
+					</button>
+				</span>
+
+			</div>
+			</div>
+			</div>
+
+</header>
+<div class="clearfix"></div>
+<!-- Header Container / End -->
+
+<!-- Titlebar
+================================================== -->
+<div id="titlebar" class="gradient">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+
+				<h2>Contact</h2>
+
+				<!-- Breadcrumbs -->
+				<nav id="breadcrumbs" class="dark">
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li>Contact</li>
+					</ul>
+				</nav>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<!-- Content
+================================================== -->
+
+
+<!-- Container -->
+<div class="container">
+	<div class="row">
+
+		<div class="col-xl-12">
+			<div class="contact-location-info margin-bottom-50">
+				<div class="contact-address">
+					<ul>
+						<li class="contact-address-headline">Our Office</li>
+						<li>Ignatius Ajuru University of Education<br>
+							Port Harcort, Rivers State,<br>
+							Nigeria.</li>
+						<li> P.M.B 5047<br> 
+							https://iauoe.edu.ng<br>
+							Cisco Academy, ID 20040066</li>
+						<li><a href="#"></a></li>
+						<li>
+							<div class="freelancer-socials">
+								<ul>
+									<li><a href="#" title="Facebook" data-tippy-placement="top"><i class="icon-brand-facebook-f"></i></a></li>
+									<li><a href="#" title="Twitter" data-tippy-placement="top"><i class="icon-brand-twitter"></i></a></li>
+									<li><a href="#" title="LinkedIn" data-tippy-placement="top"><i class="icon-brand-linkedin-in"></i></a></li>
+									<li><a href="#" title="Instagram" data-tippy-placement="top"><i class="icon-brand-instagram"></i></a></li>
+								
+								</ul>
+							</div>
+						</li>
+					</ul>
+
+				</div>
+				<div id="single-job-map-container">
+					<div id="singleListingMap" data-latitude="37.777842" data-longitude="-122.391805" data-map-icon="im im-icon-Hamburger"></div>
+					<a href="#" id="streetView">Street View</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xl-8 col-lg-8 offset-xl-2 offset-lg-2">
+
+			<section id="contact" class="margin-bottom-60">
+				<h3 class="headline margin-top-15 margin-bottom-35">Any questions? Feel free to contact us!</h3>
+
+				<form method="post" name="contactform" id="contactform" autocomplete="on">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="input-with-icon-left">
+								<input class="with-border" name="name" type="text" id="name" placeholder="Your Name" required="required" />
+								<i class="icon-material-outline-account-circle"></i>
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="input-with-icon-left">
+								<input class="with-border" name="email" type="email" id="email" placeholder="Email Address" pattern="^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$" required="required" />
+								<i class="icon-material-outline-email"></i>
+							</div>
+						</div>
+					</div>
+
+					<div class="input-with-icon-left">
+						<input class="with-border" name="subject" type="text" id="subject" placeholder="Subject" required="required" />
+						<i class="icon-material-outline-assignment"></i>
+					</div>
+
+					<div>
+						<textarea class="with-border" name="comments" cols="40" rows="5" id="comments" placeholder="Message" spellcheck="true" required="required"></textarea>
+					</div>
+
+					<input type="submit" class="submit button margin-top-15" id="submit" value="Submit Message" />
+
+				</form>
+			</section>
+
+		</div>
+
+	</div>
+</div>
+<!-- Container / End -->
+
+<!-- Footer
+================================================== -->
+<div id="footer">
+	
+	<!-- Footer Top Section -->
+	<div class="footer-top-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+
+					<!-- Footer Rows Container -->
+					<div class="footer-rows-container">
+						
+						<!-- Left Side -->
+						<div class="footer-rows-left">
+							<div class="footer-row">
+								<div class="footer-row-inner footer-logo">
+									<img src="images/unilogo.jpg" alt="">
+								</div>
+							</div>
+						</div>
+						
+						<!-- Right Side -->
+						<div class="footer-rows-right">
+
+							<!-- Social Icons -->
+							<div class="footer-row">
+								<div class="footer-row-inner">
+									<ul class="footer-social-links">
+										<li>
+											<a href="https://www.facebook.com//" title="Facebook" data-tippy-placement="bottom" data-tippy-theme="light">
+												<i class="icon-brand-facebook-f"></i>
+											</a>
+										</li>
+										<li>
+											<a href="https://www.twitter.com//" title="Twitter" data-tippy-placement="bottom" data-tippy-theme="light">
+												<i class="icon-brand-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a href="https://www.instagram.com//" title="Instagram" data-tippy-placement="bottom" data-tippy-theme="light">
+												<i class="icon-brand-instagram"></i>
+											</a>
+										</li>
+										<li>
+											<a href="https://www.linkedin.com/in//" title="LinkedIn" data-tippy-placement="bottom" data-tippy-theme="light">
+												<i class="icon-brand-linkedin-in"></i>
+											</a>
+										</li>
+									</ul>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+							
+							<!-- Language Switcher -->
+							<div class="footer-row">
+								<div class="footer-row-inner">
+									<select class="selectpicker language-switcher" data-selected-text-format="count" data-size="5">
+										<option selected>English</option>
+										<!-- <option>Français</option>
+										<option>Español</option>
+										<option>Deutsch</option> -->
+									</select>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<!-- Footer Rows Container / End -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Footer Top Section / End -->
+
+	<!-- Footer Middle Section -->
+	<div class="footer-middle-section">
+		<div class="container">
+			<div class="row">
+					<!-- Links -->
+					<div class="col-xl-2 col-lg-2 col-md-3">
+						<div class="footer-links">
+							<h3></h3>
+							<ul>
+								<li><a href="pages-register.html">Search for Hostel<span></span></a></li>
+								<li><a href="pages-register.html"><span>Book a Hostel</span></a></li>
+							</ul>
+						</div>
+					</div>
+	
+					<!-- Links -->
+					<div class="col-xl-2 col-lg-2 col-md-3">
+						<div class="footer-links">
+							<h3></h3>
+							<ul>
+								<li><a href="services.html"><span>Hostels</span></a></li>
+								<li><a href=".html"><span>Portal</span></a></li>
+							</ul>
+						</div>
+					</div>
+	
+					<!-- Links -->
+					<div class="col-xl-2 col-lg-2 col-md-3">
+						<div class="footer-links">
+							<h3>Help</h3>
+							<ul>
+								<li><a href="pages-contact.html"><span>Contact us</span></a></li>
+								<li><a href="#"><span>Terms & conditions</span></a></li>
+								<li><a href="#"><span>Feedbacks</span></a></li>
+							</ul>
+						</div>
+					</div>
+
+					<!-- Links -->
+				<div class="col-xl-2 col-lg-2 col-md-3">
+					<div class="footer-links">
+						<h3>Account</h3>
+						<ul>
+							<li><a href="pages-login.html"><span>Log In</span></a></li>
+							<li><a href="pages-register.html"><span>Register</span></a></li>
+						</ul>
+					</div>
+				</div>
+
+				<!-- Newsletter -->
+				<div class="col-xl-4 col-lg-4 col-md-12">
+					<h3><i class="icon-feather-mail"></i> Sign Up For a Newsletter</h3>
+					<p>Get notifications.</p>
+					<form action="#" method="get" class="newsletter">
+						<input type="text" name="fname" placeholder="Enter your email address">
+						<button type="submit"><i class="icon-feather-arrow-right"></i></button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Footer Middle Section / End -->
+	
+	<!-- Footer Copyrights -->
+	<div class="footer-bottom-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+					© 2021 <strong>Ignatius Ajuru University of Education</strong>. All Rights Reserved.
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Footer Copyrights / End -->
+
+</div>
+<!-- Footer / End -->
+
+</div>
+<!-- Wrapper / End -->
+
+<!-- Scripts
+================================================== -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-migrate-3.0.0.min.js"></script>
+<script src="js/mmenu.min.js"></script>
+<script src="js/tippy.all.min.js"></script>
+<script src="js/simplebar.min.js"></script>
+<script src="js/bootstrap-slider.min.js"></script>
+<script src="js/bootstrap-select.min.js"></script>
+<script src="js/snackbar.js"></script>
+<script src="js/clipboard.min.js"></script>
+<script src="js/counterup.min.js"></script>
+<script src="js/magnific-popup.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/custom.js"></script>
+
+<!-- Snackbar // documentation: https://www.polonel.com/snackbar/ -->
+<script>
+// Snackbar for user status switcher
+$('#snackbar-user-status label').click(function() { 
+	Snackbar.show({
+		text: 'Your status has been changed!',
+		pos: 'bottom-center',
+		showAction: false,
+		actionText: "Dismiss",
+		duration: 3000,
+		textColor: '#fff',
+		backgroundColor: '#383838'
+	}); 
+}); 
+</script>
+
+<!-- Google API & Maps -->
+<!-- Geting an API Key: https://developers.google.com/maps/documentation/javascript/get-api-key -->
+<script src="https://maps.googleapis.com/maps/api/js?key=&libraries=places"></script>
+<script src="js/infobox.min.js"></script>
+<script src="js/markerclusterer.js"></script>
+<script src="js/maps.js"></script>
+
+</body>
+</html>
